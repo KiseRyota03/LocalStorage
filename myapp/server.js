f9c69e2f0ff158f53b84ac61c6893051bed00cca
haohandaitruongphu <hiephomhinh55@gmail.com>
2023-08-23 20:14:56 +0700

    second commit

---
+++ myapp/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+app.use(cors({origin: true, credentials: true}));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
