5fbcba4a65d852acff6bdaebe62ce050c9e475e0
Lê Văn Phương <levanphuongd11cn4@gmail.com>
2023-12-05 15:44:47 +0700

    - chỉnh lại luồng quay video

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,12 +1,12 @@
 import './Register.scss';
-import React, { useRef, useState, useEffect, useContext } from "react";
+import React, {useRef, useState, useEffect, useContext} from "react";
 // import Layout from '../Layout';
 import Video from '../Video';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLock } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faLock} from '@fortawesome/free-solid-svg-icons'
 import Author from '../Author';
 // import axios from '~/components/api/axios'
 // import { loginApi } from '../services/userService';
@@ -21,126 +21,110 @@ function Register() {
     const [password, setPassword] = useState('');
 
     const [successs, setSuccess] = useState(false);
-   
-  
 
-    
 
-        const handlePrint = async (e) => {
-            e.preventDefault();
-            try {
-            const response = await axios.post('http://117.6.133.148:8089/api/v1/login', 
-            JSON.stringify({email,password}),
-            {
-            headers:{
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password,
-        }),      
-        }); 
-        setSuccess(true);
-        const Token = response.data.body.token.accessToken;
-        const ID = response.data.body.id;
-        localStorage.setItem('accessToken', (Token));
-        localStorage.setItem('id', (ID));
-        console.log(JSON.stringify(response?.data));
+    const handlePrint = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://117.6.133.148:8089/api/v1/login',
+                JSON.stringify({email, password}),
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                    }),
+                });
+            setSuccess(true);
+            const Token = response.data.body.token.accessToken;
+            const ID = response.data.body.id;
+            localStorage.setItem('accessToken', (Token));
+            localStorage.setItem('id', (ID));
+            console.log(JSON.stringify(response?.data));
 
-    }
-            catch(error) {
-                console.log(error);
-                alert('error')
-            }
+        } catch (error) {
+            console.log(error);
+            alert('error')
         }
+    }
 
     return (
-        <> { successs ? (
-                <Video></Video>
+        <> {successs ? (
+            <Video></Video>
         ) : (
 
-        
+
             <div className='res-wrap'>
- 
 
-            <div className="res-heading">
-                Sign in
-            </div>
 
-       
-
-          
-            <form onSubmit = {handlePrint} >
-                <div className='email-form' >
-                <div className="res-box" >
-                    <i className='res-icon' >
-                        <FontAwesomeIcon icon={faEnvelope} />
-                    </i>
-                    <input 
-                     placeholder='Email'
-                     className='res-add' 
-                     value = {email}
-                     onChange={(event) => setEmail(event.target.value)}
-                      />
-                </div>
-
-            
-                </div>
-
-                <div className="res-box res-box2">
-
-                    <i className='res-icon' >
-                        <FontAwesomeIcon icon={faLock} />
-                    </i>
-                    <input  
-                    placeholder='Password' 
-                    className='res-pass' 
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    />
-
-                </div>
-                <div className='res-forget'>
-                <a href="./"> Forgot Password?</a>
-                </div>
-                <div className='signButton'>
-
-                <button className='signButton-click'> 
-                 Sign in
-                </button>
+                <div className="res-heading">
+                    Sign in
                 </div>
 
 
-                </form  >
+                <form onSubmit={handlePrint}>
+                    <div className='email-form'>
+                        <div className="res-box">
+                            <i className='res-icon'>
+                                <FontAwesomeIcon icon={faEnvelope}/>
+                            </i>
+                            <input
+                                placeholder='Email'
+                                className='res-add'
+                                value={email}
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                        </div>
 
-               
+
+                    </div>
+
+                    <div className="res-box res-box2">
+
+                        <i className='res-icon'>
+                            <FontAwesomeIcon icon={faLock}/>
+                        </i>
+                        <input
+                            placeholder='Password'
+                            className='res-pass'
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
+
+                    </div>
+                    <div className='res-forget'>
+                        <a href="./"> Forgot Password?</a>
+                    </div>
+                    <div className='signButton'>
+
+                        <button className='signButton-click'>
+                            Sign in
+                        </button>
+                    </div>
 
 
-             
+                </form>
+
 
                 <div className='res-nav'>
-                <div className='res-nav_text'>
-                    Don't have an account yet ?
-                </div>
-                <div className='res-nav_click'>
-                    <a href="/author">Register now! </a>
+                    <div className='res-nav_text'>
+                        Don't have an account yet ?
+                    </div>
+                    <div className='res-nav_click'>
+                        <a href="/author">Register now! </a>
+                    </div>
+
                 </div>
 
-            </div>
-            
-            
+
             </div>
         )}
         </>
-        
-        
 
-          
 
-      
- 
     );
-
 
 
 }
--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,24 +1,24 @@
 import './Video.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera, faCircle } from '@fortawesome/free-solid-svg-icons';
-import { faHand } from '@fortawesome/free-solid-svg-icons';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
-import { faGear } from '@fortawesome/free-solid-svg-icons';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCamera, faCircle} from '@fortawesome/free-solid-svg-icons';
+import {faHand} from '@fortawesome/free-solid-svg-icons';
+import {faBook} from '@fortawesome/free-solid-svg-icons';
+import {faGear} from '@fortawesome/free-solid-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faFolder} from '@fortawesome/free-solid-svg-icons';
+import {faDownload} from '@fortawesome/free-solid-svg-icons';
 import Register from '../Register';
 import axios from 'axios';
 import accessToken from '../Register';
-import { useState, useCallback, useRef } from 'react';
+import {useState, useCallback, useRef, useEffect} from 'react';
 import Webcam from 'react-webcam';
 
 
 function Video() {
     //webcam
     const videoConstraints = {
-        width: { min: 420 },
-        height: { min: 630 },
+        width: {min: 420},
+        height: {min: 630},
         aspectRatio: 0.6666666667,
         facingMode: 'user',
     };
@@ -30,13 +30,13 @@ function Video() {
 
     const realFileBtn = document.getElementById("real-file");
     const customBtn = document.getElementById("custom-btn");
-  
+
     const realFile = () => {
         realFileBtn.click();
     };
 
     const handleDataAvailable = useCallback(
-        ({ data }) => {
+        ({data}) => {
             if (data.size > 0) {
                 setRecordedChunks((prev) => prev.concat(data));
             }
@@ -58,8 +58,27 @@ function Video() {
         setCapturing(false);
     }, [mediaRecorderRef, setCapturing]);
 
+    useEffect(() => {
+        if (recordedChunks.length) {
+            console.log('detected end record ', recordedChunks.length)
+            const blob = new Blob(recordedChunks, {
+                type: 'video/webm',
+            });
+            const formData = new FormData();
+            blob.lastModifiedDate = new Date();
+            blob.name = 'data.webm';
+            formData.append('file', blob);
+            axios.post('http://117.6.133.148:8089/api/v1/predict', formData).then((response) => {
+                console.log(response.data.body);
+                setPosts(response.data.body.prediction[0]);
+            });
+            setRecordedChunks([]);
+        }
+    }, [recordedChunks])
+
     const handleDownload = useCallback(() => {
         if (recordedChunks.length) {
+            console.log('download detected ', recordedChunks.length)
             const blob = new Blob(recordedChunks, {
                 type: 'video/webm',
             });
@@ -100,58 +119,52 @@ function Video() {
             setPosts(response.data.body.prediction[0]);
         });
     };
-    
+
     return (
         <div className="vid-wrap">
-        
+
             <div className="container">
                 <div className="button-wrap">
                     <div className="container-button">
-                        <div className="buttonTitle">Nhãn dự đoán:  {posts.action_name}</div>
-
-                        
-
-                       
-                   
+                        <div className="buttonTitle">Nhãn dự đoán: {posts.action_name}</div>
                         {/* <div className="buttonTitle-VN">Nhãn dự đoán: {posts.action_name}</div> */}
-               
                     </div>
                 </div>
 
                 <div className="webcam_controller">
-                    <Webcam mirrored={true} audio={false} videoConstraints={videoConstraints} ref={webcamRef} />
+                    <Webcam mirrored={true} audio={false} videoConstraints={videoConstraints} ref={webcamRef}/>
                     <div className='button-handler'>
 
-                    {capturing ? (
-                        <button onClick={handleStopCaptureClick}>
-                              <FontAwesomeIcon className="button_camera" icon={faCircle} />
-                            Stop Capture
+                        {capturing ? (
+                            <button onClick={handleStopCaptureClick}>
+                                <FontAwesomeIcon className="button_camera" icon={faCircle}/>
+                                Stop Capture
                             </button>
-                    ) : (
-                        <button onClick={handleStartCaptureClick}>
-                            <FontAwesomeIcon className="button_camera" icon={faCamera} />
-                            Start Capture
-                        </button>
-                    )}
-                       
-                       <input className="upload" id="real-file" type="file" hidden="hidden" onChange={handleApi} />
-                        
-                        <button id= "custom-btn" onClick= {realFile} className="camera_button">
+                        ) : (
+                            <button onClick={handleStartCaptureClick}>
+                                <FontAwesomeIcon className="button_camera" icon={faCamera}/>
+                                Start Capture
+                            </button>
+                        )}
+
+                        <input className="upload" id="real-file" type="file" hidden="hidden" onChange={handleApi}/>
+
+                        <button id="custom-btn" onClick={realFile} className="camera_button">
                             <i>
-                                <FontAwesomeIcon icon={faFolder} />
+                                <FontAwesomeIcon icon={faFolder}/>
                             </i>
                             Upload
 
                         </button>
-                    {recordedChunks.length > 0 && (
-                        <button onClick={handleDownload}>
-                            <i>
-                            
-                                <FontAwesomeIcon  className="button_camera" icon={faDownload} />
-                            </i>
-                            Download
-                        </button>
-                    )}
+                        {recordedChunks.length > 0 && (
+                            <button onClick={handleDownload}>
+                                <i>
+
+                                    <FontAwesomeIcon className="button_camera" icon={faDownload}/>
+                                </i>
+                                Download
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
@@ -159,36 +172,36 @@ function Video() {
                 <div className="barDown-items">
                     <a href="/Video" className="active-icon">
                         <i>
-                        <FontAwesomeIcon icon={faHand} />
+                            <FontAwesomeIcon icon={faHand}/>
                         </i>
-                        <div className='icon_text'> 
-                        Translate
+                        <div className='icon_text'>
+                            Translate
                         </div>
                     </a>
                     <a href="/Text" className="nonActive-icon">
-                    <i>
-                        <FontAwesomeIcon icon={faSearch} />
+                        <i>
+                            <FontAwesomeIcon icon={faSearch}/>
                         </i>
-                        <div className='icon_text'> 
-                        Search
+                        <div className='icon_text'>
+                            Search
                         </div>
                     </a>
                     <a href="/Lesson" className="nonActive-icon">
-                    <i>
-                        <FontAwesomeIcon icon={faBook} />
+                        <i>
+                            <FontAwesomeIcon icon={faBook}/>
                         </i>
-                        <div className='icon_text'> 
-                        Learn
+                        <div className='icon_text'>
+                            Learn
                         </div>
 
                     </a>
-                    
+
                     <a href="/Profile" className="nonActive-icon">
                         <i>
-                        <FontAwesomeIcon icon={faGear} />
+                            <FontAwesomeIcon icon={faGear}/>
                         </i>
-                        <div className='icon_text'> 
-                        Settings
+                        <div className='icon_text'>
+                            Settings
                         </div>
                     </a>
                 </div>
